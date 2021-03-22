21d907b7fc98056da586d7597f18c0c122574e5e
Takumi Maruyama <satouredstone@gmail.com>
2021-03-22 22:45:29 +0900

    Update configurations

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: {
     "popup-index": "./src/popup-index.tsx",
     "page-action": "./src/page-action.ts",
-    "settings-page": "./src/settings-index.tsx",
+    "options-page": "./src/options-index.tsx",
   },
   devtool: "source-map",
   // ファイルの出力設定
