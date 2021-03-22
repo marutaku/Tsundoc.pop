1d98aaf0c4935b712a52337541753a1b0af7916a
Takumi Maruyama <satouredstone@gmail.com>
2021-03-22 22:35:57 +0900

    Add settings page source

---
+++ src/settings-index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./popup/index.css";
+import App from "./settings/App";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/settings/App.tsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@material-ui/core";
+import React from "react";
+
+export const App = () => {
+  return (
+    <Container>
+      <Typography variant="h3">設定</Typography>
+    </Container>
+  );
+};
+
+export default App;
