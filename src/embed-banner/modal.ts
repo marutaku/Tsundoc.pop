3aacc182053665ad12b88e4e990ef7b1fca729f7
sh05 <shogonakamoto0107@gmail.com>
2021-03-26 18:07:23 +0900

    add image and btn label

--- src/embed-banner/modal.ts
+++ src/embed-banner/modal.ts
@@ -5,13 +5,20 @@ import { loadHTML, createStyleSheetLink, randomChoice } from "./utils";
 function injectContent(
   element: Element,
   title: string,
-  subtitle: string
+  subtitle: string,
+  // image?: stringではだめだった
+  image: any,
 ): Element {
   const titleElement = element.querySelector("#modal-title");
   const subtitleElement = element.querySelector("#modal-subtitle");
-  if (titleElement && subtitleElement) {
+  const closeBtnElement = element.querySelector("#modal-close");
+  const imgElement = element.querySelector("div#modal-img img");
+  // const imgElement = document.getElementById("#modal-img");
+  if (titleElement && subtitleElement && closeBtnElement && imgElement) {
+    imgElement.setAttribute('src', image);
     titleElement.innerHTML = title;
     subtitleElement.innerHTML = subtitle;
+    closeBtnElement.innerHTML = "この積読を消化せずにブラウジングを続ける";
   } else {
     throw new Error(`Unexpected Element.`);
   }
@@ -25,7 +32,7 @@ export async function initModal(books: Book[], bodyElement: Element) {
   bodyElement.appendChild(modal);
   bodyElement.insertBefore(linkElement, bodyElement.firstChild);
   const firstBook = randomChoice(books);
-  injectContent(modal, firstBook.title, firstBook.authors.join(","));
+  injectContent(modal, firstBook.title, firstBook.authors.join(","), firstBook.image);
   //   MicroModal.init();
   const dialog = document.querySelector("dialog");
   if (!dialog) {
